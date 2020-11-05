24c1f0eed0a6ace1f2315660315eeb9e97ea1749
Michał Kowalski <michal.kowalski@microsoft.wsei.edu.pl>
2020-11-05 13:06:58 +0100

    Add project files.

---
+++ WebApplication3/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Components
+{
+    public class NavigationMenuViewComponent:ViewComponent
+    {
+        private readonly IProductRepository _productRepository;
+        public NavigationMenuViewComponent(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_productRepository.Products.Select(x =>x.Category).Distinct().OrderBy(x => x));
+        }
+
+    }
+}
---
+++ WebApplication3/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> {
+            new Product {ID=1, Name = "AA",Description="aaaaaa",Price=12,Category="XYZ"},
+            new Product {ID=2, Name = "BB",Description="bbbbbb",Price=11,Category="XYZ"},
+            new Product {ID=3, Name = "CC",Description="cccccc",Price=9,Category="XYZ"},
+            new Product {ID=4, Name = "DD",Description="dddddd",Price=15,Category="XYZ"},
+        }.AsQueryable<Product>();
+    }
+}
