6ef05ba282039227ebcb8a9482aadb14616de996
Michał Kowalski <michal.kowalski@microsoft.wsei.edu.pl>
2021-01-21 14:19:00 +0100

    lab5

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,21 +22,26 @@ namespace WebApplication3
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddTransient<IProductRepository, EFProductRepository>();
+
             services.AddDbContext<AppDbContext>(options =>options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddTransient<IProductRepository, EFProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
             app.UseDeveloperExceptionPage(); // informacje szczegó³owe o b³êdach
             app.UseStatusCodePages(); // Wyœwietla strony ze statusem b³êdu
             app.UseStaticFiles(); // obs³uga treœci statycznych css, images, js
+            app.UseElapsedTimeMiddleware();
+            //app.UseAuthentication();
             app.UseRouting();
             app.UseEndpoints(routes =>
             {
@@ -57,6 +58,15 @@ namespace WebApplication3
                     controller = "Product",
                     action = "List",
                 });
+                routes.MapControllerRoute(
+                name: null,
+                pattern: "Admin/{action}",
+                defaults: new
+                {
+                controller = "Admin",
+                action = "Index"
+                });
+
             });
             SeedData.EnsurePopulated(app);
         }
